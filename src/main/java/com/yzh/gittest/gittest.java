ed5978c323424b938745f4a32b8d57531ed0d2bc
Anez <Anez@yuzihao.com>
2023-01-13 17:53:06 +0800

    first commit

---
+++ src/main/java/com/yzh/gittest/gittest.java
@@ -0,0 +1,12 @@
+package com.yzh.gittest;
+
+/**
+ * 1. @description:
+ * 2. @author: Anez
+ * 3. @time: 2023/1/13
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
