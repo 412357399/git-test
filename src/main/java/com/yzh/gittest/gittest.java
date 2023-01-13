b82598e0f4cab05cdd34effabd9de461d491ea63
Anez <Anez@yuzihao.com>
2023-01-13 18:01:14 +0800

    second commit

--- src/main/java/com/yzh/gittest/gittest.java
+++ src/main/java/com/yzh/gittest/gittest.java
@@ -8,5 +8,6 @@ package com.yzh.gittest;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("hello,git2");
     }
 }
